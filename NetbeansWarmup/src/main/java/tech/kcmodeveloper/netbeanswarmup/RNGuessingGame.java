bc5d2c44e2e716181485564889c9263cf4671f18
kcmodev <shchristensen3@gmail.com>
2019-12-23 10:51:53 -0600

    updated and working, still needs error handling

--- NetbeansWarmup/src/main/java/tech/kcmodeveloper/netbeanswarmup/RNGuessingGame.java
+++ NetbeansWarmup/src/main/java/tech/kcmodeveloper/netbeanswarmup/RNGuessingGame.java
@@ -13,14 +13,64 @@ public class RNGuessingGame {
         Scanner userInput = new Scanner(System.in);
         
         System.out.println("Let's play a guessing game...");
-        System.out.print("\nPick a number between 1 - 500: ");
+        System.out.print("\nPick a number over 100 : ");
         
         int userChoice = userInput.nextInt();
         
-        if (userChoice >= 1 && userChoice <= 500){
+        if (userChoice >= 100){
+            System.out.println("Your number is between 1 and " + userChoice + ".");
+            int answer = rand.nextInt(userChoice);
             
+            int counter = 1;
+            int userGuess;
+            
+            System.out.println("Time to start guessing...");
+            
+            while (true){
+                System.out.println("Answer: " + answer);
+                
+                if (counter == 1){
+                System.out.println("Enter your first guess: ");
+                userGuess = userInput.nextInt();
+                        if (userGuess > answer){
+                            System.out.println("Too high! Try again.");
+                            counter++;
+                        } else if (userGuess < answer){
+                            System.out.println("Too low! Try again.");
+                            counter++;
+                        } else if (userGuess == answer) {
+                            System.out.println("Congrats it only took you one guess!!");
+                            break;
+                        } else {
+                            System.out.println("Invalid input.");
+                        }
+                } else if (counter > 1){
+                    System.out.println("Enter your next guess: ");
+                    userGuess = userInput.nextInt();
+                    
+                    if (userGuess == answer){
+                        System.out.println("You got it!");
+                        System.out.println("You got it! It took you " + counter + " guesses.");
+                        break;
+                    } else if(userGuess != answer){
+                        if (userGuess > answer){
+                            System.out.println("Too high! Try again.");
+                            counter++;
+                        } else if (userGuess < answer){
+                            System.out.println("Too low! Try again.");
+                            counter++;
+                        } else {
+                            System.out.println("Invalid response.");
+                        }
+                    } else {
+                        System.out.println("Invalid response.");
+                    }
+                }else {
+                    System.out.println("Invalid response.");
+                }
+            }
         } else {
-            System.out.println("Invalid number. Please try again.");
+            System.out.println("Invalid response.");
         }
     }
 }
